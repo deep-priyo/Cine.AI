b12895a2db4666ea1febe1a3e5344493405edfc2
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-21 17:47:03 +0530

    setting up styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,27 @@ import Movies from "./Movies/Movies.jsx";
 import MovieInformation from "./MovieInformation/MovieInformation.jsx";
 import Actors from "./Actors/Actors.jsx";
 import Profile from "./Profile/Profile.jsx";
-
+import NavBar from "./NavBar/NavBar.jsx";
+import useStyles from "./styles";
 const App = () => {
+    const classes = useStyles();
     return (
-        <BrowserRouter>
+        <div className={classes.root}>
+            <BrowserRouter>
             <CssBaseline />
-            <main>
+            <NavBar/>
+            <main className={classes.content}>
+                <div className={classes.toolbar}/>
                 <Routes>
-                    <Route path="/" element={<h1>Home</h1>} />
-                    <Route path="/movies" element={<Movies />} />
+                    <Route path="/" element={<Movies />} />
                     <Route path="/movies/:id" element={<MovieInformation/>} />
                     <Route path="/actors/:id" element={<Actors/>} />
                     <Route path="/profile/:id" element={<Profile/>} />
                 </Routes>
             </main>
         </BrowserRouter>
+        </div>
+
     );
 };
 
